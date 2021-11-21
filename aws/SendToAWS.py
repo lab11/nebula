68c0d0002e7e4db2289b7c19c4a2d02e32765c48
tessxd <tdespres@berkeley.edu>
2021-11-20 23:45:12 -0800

    updated python code to send data to aws and parse to DynamoDB

--- aws/SendToAWS.py
+++ aws/SendToAWS.py
@@ -1,15 +1,24 @@
 import os
 import csv
+import json
 
-data = ''
+data_from_csv = []
 with open('nRf_data.csv', 'r') as csvfile:
     csvreader = csv.reader(csvfile)
-    for row in csvreader:
-        print(row)
-        data += str(row)
+    for i,row in enumerate(csvreader):
+        #print(row)
+        if (i >= 1):
+            data_from_csv.append(row[1])
 
-print(data)
+#print(data_from_csv)
 
+data = {
+    "sensor_id":0,
+    "mule_id":0,
+    "data":int(data_from_csv[0]) #eventually this should be changed to handle multiple data packets
+}
 
 #send data over
-os.system('python3 ../../../../../../aws-iot-device-sdk-python-v2/samples/pubsub.py --topic topic_1 --root-ca ~/certs/Amazon-root-CA-1.pem --cert ~/certs/device.pem.crt --key ~/certs/private.pem.key --endpoint a3gshqjfftdu7n-ats.iot.us-west-1.amazonaws.com --message "{}" --count 3'.format(str(data)))
+command = "python3 GalaxyAWS.py --topic topic_1 --root-ca ~/certs/Amazon-root-CA-1.pem --cert ~/certs/device.pem.crt --key ~/certs/private.pem.key --endpoint a3gshqjfftdu7n-ats.iot.us-west-1.amazonaws.com --message '{}' --count 3".format(json.dumps(data))
+print(command)
+os.system(command)
