028d137bafc4d6bea470e05bef6c0d4863531c9c
Ubuntu <ubuntu@ip-172-31-31-171.us-west-1.compute.internal>
2021-12-01 01:55:35 +0000

    plot script and latency/throughput graphs

--- aws/system_plots.py
+++ aws/system_plots.py
@@ -8,20 +8,23 @@ sns.set_theme()
 #import data and sanity check print
 plaintext_df = pd.read_csv('latency.csv',names=["num_mules","latency"])
 print(plaintext_df.head(10))
+express_df = pd.read_csv('express-stats.csv',names=["num_mules","latency","throughput"])
 
 #plot latency
 plt.figure(1)
-sns.lineplot(x=plaintext_df.num_mules,y=plaintext_df.latency)
-plt.ylim(1,3)
+sns.lineplot(x=plaintext_df.num_mules,y=plaintext_df.latency,label="plaintext")
+sns.lineplot(x=express_df.num_mules,y=express_df.latency,label="express")
+#plt.ylim(1,3)
 plt.title("Latency vs. Parallel Messages")
 plt.xlabel("Number of Mules Sending in Parallel")
 plt.ylabel("Latency (s)")
-plt.savefig('latency.jpg') #change to svg to generate higher fidelity plots
+plt.savefig('latency.pdf') #change to svg to generate higher fidelity plots
 
 #plot throughput 
 plt.figure(2)
-sns.lineplot(x=plaintext_df.num_mules,y=(plaintext_df.num_mules/plaintext_df.latency))
+sns.lineplot(x=plaintext_df.num_mules,y=(plaintext_df.num_mules/plaintext_df.latency),label="plaintext")
+sns.lineplot(x=express_df.num_mules,y=express_df.throughput,label="express")
 plt.title("Throughput vs. Parallel Messages")
 plt.xlabel("Number of Mules Sending in Parallel")
 plt.ylabel("Throughput (msgs/sec)")
-plt.savefig('throughput.jpg') #change to svg to generate higher fidelity plots
+plt.savefig('throughput.pdf') #change to svg to generate higher fidelity plots
