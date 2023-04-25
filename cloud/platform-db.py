944e2cfba2a9b8007fa698984cea2532a4084a35
Shishir Patil <shishirpatil@berkeley.edu>
2023-04-24 21:53:49 -0700

    increment logic mods

--- cloud/platform-db.py
+++ cloud/platform-db.py
@@ -1,6 +1,8 @@
 import sqlite3
 from concurrent.futures import ProcessPoolExecutor
 
+DEBUG = True
+
 class KeyValueDatabase:
     def __init__(self, db_name='mules.db'):
         self.db_name = db_name
@@ -14,18 +16,18 @@ class KeyValueDatabase:
                             )''')
             conn.commit()
 
-    def increment_count(self, mule_id):
+    def increment_count(self, mule_id, increment):
         with sqlite3.connect(self.db_name) as conn:
             conn.execute('''INSERT OR IGNORE INTO mules (mule_id, count)
                             VALUES (?, 0)''', (mule_id,))
             conn.execute('''UPDATE mules
-                            SET count = count + 1
-                            WHERE mule_id = ?''', (mule_id,))
+                            SET count = count + ?
+                            WHERE mule_id = ?''', (increment, mule_id))
             conn.commit()
 
-    def batch_increment_counts(self, mule_ids):
+    def batch_increment_counts(self, mule_id_increments):
         with ProcessPoolExecutor(max_workers=32) as executor:
-            executor.map(self.increment_count, mule_ids)
+            executor.map(lambda x: self.increment_count(x[0], x[1]), mule_id_increments)
 
     def get_counts(self):
         with sqlite3.connect(self.db_name) as conn:
@@ -33,12 +35,12 @@ class KeyValueDatabase:
                                      FROM mules''')
             return {row[0]: row[1] for row in cursor}
 
-if __name__ == '__main__':
+if DEBUG:
     db = KeyValueDatabase()
 
     # Increment counts for mule_ids
-    mule_ids = ['mule_1', 'mule_2', 'mule_3', 'mule_2']
-    db.batch_increment_counts(mule_ids)
+    mule_id_increments = [('mule_1', 2), ('mule_2', 3), ('mule_3', 1)]
+    db.batch_increment_counts(mule_id_increments)
 
     # Retrieve the current counts
     print(db.get_counts())
