f53c3b81769dd214f37c6910eaad6d65c35eb464
Shishir Patil <shishirpatil@berkeley.edu>
2023-04-25 13:04:52 -0700

    AES on nRF

---
+++ sensor/app/aes-main-test.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "aes_gcm.h"
+
+int main(void)
+{
+    uint8_t key[NRF_CRYPTO_AES_KEY_SIZE] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+                                            0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F};
+    uint8_t iv[NRF_CRYPTO_AES_IV_SIZE] = {0xA0, 0xA1, 0xA2, 0xA3, 0xA4, 0xA5, 0xA6, 0xA7,
+                                          0xA8, 0xA9, 0xAA, 0xAB};
+
+    uint8_t plaintext[] = "Your message here!";
+    size_t length = sizeof(plaintext) - 1; // Subtract 1 to ignore the null-terminator
+    uint8_t ciphertext[length];
+    uint8_t tag[NRF_CRYPTO_AES_IV_SIZE];
+
+    // Encrypt the character array
+    encrypt_character_array(key, iv, plaintext, ciphertext, tag, length);
+
+    // Print the encrypted data
+    printf("Encrypted data: ");
+    for (size_t i = 0; i < length; i++)
+    {
+        printf("%02x ", ciphertext[i]);
+    }
+    printf("\n");
+
+    // Print the authentication tag
+    printf("Authentication tag: ");
+    for (size_t i = 0; i < NRF_CRYPTO_AES_IV_SIZE; i++)
+    {
+        printf("%02x ", tag[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
+                                         
+
