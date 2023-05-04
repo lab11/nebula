413123709711dbeb60c1614b23d506ff84daecf5
Shishir Patil <shishirpatil@berkeley.edu>
2023-05-04 05:34:08 +0000

    producer relationship

--- cloud/provider.py
+++ cloud/provider.py
@@ -3,6 +3,10 @@ import tokenlib
 import util
 import platform_db
 import platform_tokendb
+import queue
+import threading
+from concurrent.futures import ThreadPoolExecutor
+
 
 # load keypair (generated with gen_keypair.py)
 with open('keypair.bin', 'rb') as f:
@@ -24,10 +28,43 @@ def sign_tokens(payload) -> list[str]:
     return [util.encode_bytes(token) for token in signed_tokens]
 
 
+def consumer(batch_queue):
+    batch_size = 14000  # Define a batch size for DB writes
+    batch = []
+
+    while True:
+        token = batch_queue.get()
+
+        if token is None:
+            # Put the sentinel back for other potential consumers
+            batch_queue.put(None)
+            break
+
+        batch.append(token)
+
+        if len(batch) >= batch_size:
+            num_unused = token_db.add_new_elements(batch)
+            #mule_db.batch_increment_counts([('mule', num_unused)])
+            batch = []
+
+    # Write any remaining elements in the batch
+    if batch:
+        num_unused = token_db.add_new_elements(batch)
+
 def redeem_tokens(payload) -> int:
+    # Define a queue and start the consumer
+    batch_queue = queue.Queue()
+    consumer_thread = threading.Thread(target=consumer, args=(batch_queue,), daemon=True)
+    consumer_thread.start()
+
     decoded_tokens = [util.decode_bytes(token) for token in payload["tokens"]]
     valid_tokens = [token for token in decoded_tokens if tokenlib.verify_token(_keypair, token)]
-    num_unused = token_db.add_new_elements(valid_tokens)
-    # mule_db.batch_increment_counts([('mule', num_unused)])
-    return num_unused
 
+    # Put valid tokens into the queue
+    for token in valid_tokens:
+        batch_queue.put(token)
+
+    # Signal to the consumer that the producer has finished
+    batch_queue.put(None)
+
+    return 0
